c085c91d08fedb4d8422cfb0a8a851ecfef87c29
Kholid <114975624+MKsafr@users.noreply.github.com>
2024-06-11 20:57:47 +0800

    Add files via upload

---
+++ Modul 3/Unscramble/app/src/main/java/com/example/android/unscramble/ui/game/ListofWords.kt
@@ -0,0 +1,35 @@
+package com.example.android.unscramble.ui.game
+
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+val allWordsList: List<String> =
+    listOf("hewan", "otomatis", "anekdot", "alpabet", "semua", "keren",
+        "timbul", "balon", "ranjang", "bangku", "terbaik", "lahir",
+        "buku", "koper", "kamera", "kemah", "lilin", "kucing",
+        "kol", "pesan", "anak", "kelas", "klasik", "ruang",
+        "kopi", "warna", "kukis", "kreatif", "layar", "tari", "siang",
+        "dinosaurus", "gagang", "makan", "mimpi", "senja", "eating",
+        "gajah", "zamrud", "seram", "elektrik", "selesai", "bunga",
+        "ikuti", "rubah", "bingkai", "gratis", "sering", "corong",
+        "hijau", "gitar", "swalayan", "kaca", "hebat", "sedih",
+        "pangkas", "setengah", "asli", "terjadi", "madu", "cepat",
+        "ratus", "es", "salju", "investasi", "undang", "ikon", "kenal",
+        "canda", "riang", "jurnal", "lompat", "ikut", "kanguru",
+        "ketik", "dapur", "koala", "baik", "kaledoskop", "baring",
+        "telat", "tawa", "belajar", "lemon", "surat", "lily",
+        "koran", "marinir", "marsmallow", "labirin", "meditasi", "melodi",
+        "menit", "monumen", "bulan", "motor", "gunung", "musik",
+        "utara", "hidung", "malam", "nama", "mustahil", "nego", "nomor",
+        "lawan", "gurita", "pohon", "pesanan", "buka", "kutub",
+        "paket", "lukisan", "orang", "piknik", "bantal", "pizza", "siniar",
+        "presentasi", "anjing", "bingung", "resep", "lepas", "resto",
+        "putar", "ulang", "kamar", "lari", "rahasia", "bibit",
+        "kapal", "baju", "harus", "kecil", "roket", "bintang",
+        "mampu", "jalan", "gaya", "terbit", "taksi", "rapi", "waktu",
+        "bersama", "gigi", "turis", "pergi", "truk", "bawah", "berguna",
+        "kuda", "unik", "angkat", "seragam", "pot", "biola", "datang",
+        "lihat", "volume", "pandang", "segel", "kelana", "dunia",
+        "dingin", "sehat", "angin", "x-ray", "gambang", "yoga",
+        "yogurt", "yoyo", "kamu", "tahun", "enak", "zebra",
+        "zigzag", "kebun", "zona", "tekun")
